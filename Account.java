e906d6b6c8c3d056fa4c8548211c9fbec221ca39
mkuehl <maxmimilianlkuehl@web.de>
2015-10-16 12:47:47 +0200

    Variant03

--- Account.java
+++ Account.java
@@ -12,7 +12,7 @@ int balance = 0;
 	/*@ 
 	 ensures (!\result ==> balance == \old(balance)) 
 	  && (\result ==> balance == \old(balance) + x); @*/
-	 private boolean  update__wrappee__BankAccount  (int x) {
+	boolean update(int x) {
 		int newBalance = balance + x;
 		if (newBalance < OVERDRAFT_LIMIT)
 			return false;
@@ -21,27 +21,9 @@ int balance = 0;
 	}
 
 	/*@ 
-	 ensures ( (!\result ==> balance == \old(balance)) 
-	  && (\result ==> balance == \old(balance) + x) ) 
-	  && (!\result ==> withdraw == \old(withdraw)) 
-	  && (\result ==> withdraw <= \old(withdraw)); @*/
-	boolean update(int x) {
-		int newWithdraw = withdraw;
-		if (x < 0)  {
-			newWithdraw += x;
-			if (newWithdraw < DAILY_LIMIT) 
-				return false;
-		}
-		if (!update__wrappee__BankAccount(x))
-			return false;
-		withdraw = newWithdraw;
-		return true;
-	}
-
-	/*@ 
 	 ensures (!\result ==> balance == \old(balance)) 
 	  && (\result ==> balance == \old(balance) - x); @*/
-	 private boolean  undoUpdate__wrappee__BankAccount  (int x) {
+	boolean undoUpdate(int x) {
 		int newBalance = balance - x;
 		if (newBalance < OVERDRAFT_LIMIT)
 			return false;
@@ -49,28 +31,16 @@ int balance = 0;
 		return true;
 	}
 
+	final static int INTEREST_RATE = 2;
+
+	int interest = 0;
+
 	/*@ 
-	 ensures ( (!\result ==> balance == \old(balance)) 
-	  && (\result ==> balance == \old(balance) - x) ) 
-	  && (!\result ==> withdraw == \old(withdraw)) 
-	  && (\result ==> withdraw >= \old(withdraw)); @*/
-	boolean undoUpdate(int x) {
-		int newWithdraw = withdraw;
-		if (x < 0)  {
-			newWithdraw -= x;
-			if (newWithdraw < DAILY_LIMIT) 
-				return false;
-		}
-		if (!undoUpdate__wrappee__BankAccount(x))
-			return false;
-		withdraw = newWithdraw;
-		return true;
+	 ensures (balance >= 0 ==> \result >= 0) 
+	   && (balance <= 0 ==> \result <= 0); @*/
+	 /*@pure@*/  int calculateInterest() {
+		return balance * INTEREST_RATE / 36500;
 	}
-
-	final static int DAILY_LIMIT = -1000;
-
-	//@ invariant withdraw >= DAILY_LIMIT;
-int withdraw = 0;
 
 
 }
--- Application.java
+++ Application.java
@@ -8,14 +8,25 @@ Account account = new Account();
 	}
 
 	/*@ 
-	 ensures account.withdraw == 0; @*/
+	 ensures true 
+	  && (account.balance >= 0 ==> account.interest >= \old(account.interest)) 
+	  && (account.balance <= 0 ==> account.interest <= \old(account.interest)); @*/
 	void nextDay() {
 		nextDay__wrappee__BankAccount();
-		account.withdraw = 0;
+		account.interest += account.calculateInterest();
 	}
 
 
+	 private void  nextYear__wrappee__BankAccount  () {
+	}
+
+	/*@ 
+	 ensures account.balance == \old(account.balance) + \old(account.interest) 
+	  && account.interest == 0; @*/
 	void nextYear() {
+		nextYear__wrappee__BankAccount();
+		account.balance += account.interest;
+		account.interest = 0;
 	}
 
 
