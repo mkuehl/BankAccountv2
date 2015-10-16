a7a6183a6272d60ddcf723781bb495a2cb95359d
mkuehl <maxmimilianlkuehl@web.de>
2015-10-16 12:46:26 +0200

    first commit with variant01

---
+++ Application.java
@@ -0,0 +1,15 @@
+package bankaccount; public class Application {
+	//@ invariant account != null;
+Account account = new Account();
+
+	/*@ 
+	 requires true; @*/
+	void nextDay() {
+	}
+
+
+	void nextYear() {
+	}
+
+
+}
